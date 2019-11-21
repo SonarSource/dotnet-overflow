6d774a4c1f672d1d9817418254b842e39c58bdf2
Loïc Joly <loic.joly@sonarsource.com>
2019-11-21 09:47:18 +0100

    First commit

---
+++ ApplicationWithOverflow/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ApplicationWithOverflow
+{
+    class Program
+    {
+        class A
+        {
+            public int ovflw1()
+            {
+                unchecked
+                {
+                    int i = 1834567890 + 1834567890;
+                    return i;
+                }
+            }
+
+            public int ovflw2()
+            {
+                int i = 1834567890;
+                i += i;
+                return i;
+            }
+
+            public int ovflw3()
+            {
+                int i = 1834567890;
+                var j = i + i;
+                return j;
+            }
+
+            public int ovflw4()
+            {
+                int i = -1834567890;
+                int j = 1834567890;
+                var k = i - j;
+                return k;
+            }
+
+            public int ovflw5(int i)
+            {
+                if (i > 1834567890)
+                {
+                    return i + i;
+                }
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
